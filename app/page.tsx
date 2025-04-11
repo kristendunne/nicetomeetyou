65f837f8bbcb2909f8a9775e2c5288e68d88aba8
Kristen Dunne <kristendunne@gmail.com>
2025-04-11 11:18:23 -0500

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,7 @@ export default function Home() {
             <li>Known for building rapport and trust - both internally and externally</li>
           </ul>
         </div>
+        console.log("motion object:", motion);
 
         <div className="mb-6">
           <motion.h2
@@ -54,61 +55,61 @@ export default function Home() {
             transition={{ staggerChildren: 0.1 }}
             viewport={{ once: true }}
           >
-            <motion.p
+            <motion.div
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.4 }}
             >
               I currently live in Chicago - constantly torn between being a city girl or a nature girl at heart. I love the energy of the city but feel most at peace surrounded by trees, trails, or a body of water.
-            </motion.p>
+            </motion.div>
 
-            <motion.p
+            <motion.div
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.4 }}
             >
               I have an ever-growing love for moving and taking care of my body and mind, and I’m always finding new areas where I’m capable. I’m just a couple of short weeks away from finishing my <strong>Yoga Teacher Training</strong> (which has taken up loads of life lately in all the best ways!) and I can’t wait to kick off this side hustle! 🧘‍♀️
-            </motion.p>
+            </motion.div>
 
-            <motion.p
+            <motion.div
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.4 }}
             >
               I’m also working toward a <strong>certification in Nutrition</strong>, which feels like a natural next step. You’ll find me in the gym daily—unless it’s warm season in Chicago, in which case I’ll be soaking up as much sun as possible training for triathlons. I’ve been racing for years and might never pass the “amateur” title… but it’s a fun challenge :) My bike might be my best friend.
-            </motion.p>
+            </motion.div>
 
-            <motion.p
+            <motion.div
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.4 }}
             >
               I just got back from a short road trip to <strong>Shawnee National Forest</strong> with my boyfriend (Brian) and our doggo (Nitro). We try to adventure when and where we can—travel is such a special treat, whether with company or when riding solo.
-            </motion.p>
+            </motion.div>
 
-            <motion.p
+            <motion.div
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.4 }}
             >
               We share our home with our two adorable cats, <strong>Alice</strong> and <strong>Morty</strong>, and we’re still (a year later!) taking suggestions on how to fully unlock the cats ↔ dog bond... we feel like we’ve tried it all. 🐱🐱🐶
-            </motion.p>
+            </motion.div>
 
-            <motion.p
+            <motion.div
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.4 }}
             >
               I’m a sucker for time with friends and a good event -<strong>live music</strong>, podcast recordings, markets, and everything in between. I also love creative outlets to balance out my work brain and spend time with activities like wheel throwing and getting dirty with clay - also “amateur potter” 😄).
-            </motion.p>
+            </motion.div>
 
-            <motion.p
+            <motion.div
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.4 }}
             >
               I genuinely love meeting all kinds of people, and I hope for the chance to get to know more of you!
-            </motion.p>
+            </motion.div>
           </motion.div>
         </div>
 
